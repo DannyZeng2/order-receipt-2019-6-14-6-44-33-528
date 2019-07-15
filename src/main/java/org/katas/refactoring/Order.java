8742687fcfafa04472b25db1725740cafdd775a3
Danny Zeng <DANNY.ZENG@oocl.com>
2019-07-15 18:13:50 +0800

    test passed

--- src/main/java/org/katas/refactoring/Order.java
+++ src/main/java/org/katas/refactoring/Order.java
@@ -3,18 +3,18 @@ package org.katas.refactoring;
 import java.util.List;
 
 public class Order {
-    String customer;
+    String customerName;
     String address;
     List<LineItem> lineItems;
 
-    public Order(String customer, String address, List<LineItem> lineItems) {
-        this.customer = customer;
+    public Order(String customerName, String address, List<LineItem> lineItems) {
+        this.customerName = customerName;
         this.address = address;
         this.lineItems = lineItems;
     }
 
     public String getCustomerName() {
-        return customer;
+        return customerName;
     }
 
     public String getCustomerAddress() {
--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -1,17 +1,17 @@
 package org.katas.refactoring;
 
 /**
- * OrderReceipt prints the details of order including customer name, address, description, quantity,
+ * OrderReceipt prints the details of order including customerName name, address, description, quantity,
  * price and amount. It also calculates the sales tax @ 10% and prints as part
  * of order. It computes the total order amount (amount of individual lineItems +
  * total sales tax) and prints it.
  */
 public class OrderReceipt {
     public static final double SALES_TAX_RATE = .10;
-    private Order o;
+    private Order order;
 
-    public OrderReceipt(Order o) {
-        this.o = o;
+    public OrderReceipt(Order order) {
+        this.order = order;
     }
 
     public String printReceipt() {
@@ -22,7 +22,7 @@ public class OrderReceipt {
 
         double totSalesTx = 0d;
         double tot = 0d;
-        for (LineItem lineItem : o.getLineItems()) {
+        for (LineItem lineItem : order.getLineItems()) {
 
             appendLineItem(output, lineItem);
 
@@ -42,8 +42,8 @@ public class OrderReceipt {
     }
 
     private void printCustomerNameAndAddress(StringBuilder output) {
-        output.append(o.getCustomerName());
-        output.append(o.getCustomerAddress());
+        output.append(order.getCustomerName());
+        output.append(order.getCustomerAddress());
     }
 
     private void printHeaders(StringBuilder output) {
